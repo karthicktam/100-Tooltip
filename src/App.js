bbeb5f96487675044461a18ddcdbc684e8ce8fcb
karthicktamil <43010369+karthicktam@users.noreply.github.com>
2020-08-28 14:59:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <button className="btn">
+      Press
+      <span className="tooltip">Don't press</span>
+    </button>
+  );
+}
